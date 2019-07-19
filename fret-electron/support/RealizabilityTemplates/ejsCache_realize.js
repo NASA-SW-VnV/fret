39e05a3e6812c8d551776466fa76a2b44dd74314
Kooi <dkooi@rse-mac-2.ndc.nasa.gov>
2019-07-19 11:45:10 -0700

    Created RealizabilityTemplates and modified CocoSpec templates for use with JKind2. Added additional template generation decleration to VariablesView.

---
+++ fret-electron/support/RealizabilityTemplates/ejsCache_realize.js
@@ -0,0 +1,19 @@
+const contractSignature = require('ejs-compiled-loader!./ComponentSignature.ejs'),
+      internalVariables = require('ejs-compiled-loader!./InternalVariables.ejs'),
+      internalAssignments = require('ejs-compiled-loader!./InternalAssignments.ejs'),
+      modeDefinitions = require('ejs-compiled-loader!./ModeDefinitions.ejs'),
+      propertyDefinitions = require('ejs-compiled-loader!./PropertyDefinitions.ejs'),
+      complete = require('ejs-compiled-loader!./Complete.ejs');
+
+exports.renderRealizeCode = () => {
+  return {
+    contract: {
+      componentSignature: componentSignature,
+      internalVariables: internalVariables,
+      internalAssignments: internalAssignments,
+      modeDefinitions: modeDefinitions,
+      propertyDefinitions: propertyDefinitions,
+      complete: complete
+    }
+  };
+};
