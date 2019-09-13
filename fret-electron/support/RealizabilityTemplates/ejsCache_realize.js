80337105219501b7562b04a9501b2e5c77f33415
Kooi <dkooi@rse-mac-2.ndc.nasa.gov>
2019-09-12 17:46:09 -0700

    Removed mode definition from realizability templates.

--- fret-electron/support/RealizabilityTemplates/ejsCache_realize.js
+++ fret-electron/support/RealizabilityTemplates/ejsCache_realize.js
@@ -3,7 +3,6 @@ const componentSignature = require('ejs-compiled-loader!./ComponentSignature.ejs
       delayOperators = require('ejs-compiled-loader!./DelayOperators.ejs'),
       internalVariables = require('ejs-compiled-loader!./InternalVariables.ejs'),
       internalAssignments = require('ejs-compiled-loader!./InternalAssignments.ejs'),
-      modeDefinitions = require('ejs-compiled-loader!./ModeDefinitions.ejs'),
       propertyDefinitions = require('ejs-compiled-loader!./PropertyDefinitions.ejs'),
       complete = require('ejs-compiled-loader!./Complete.ejs');
 
@@ -15,7 +14,6 @@ exports.renderRealizeCode = () => {
       componentSignature: componentSignature,
       internalVariables: internalVariables,
       internalAssignments: internalAssignments,
-      modeDefinitions: modeDefinitions,
       propertyDefinitions: propertyDefinitions,
       complete: complete
     }
