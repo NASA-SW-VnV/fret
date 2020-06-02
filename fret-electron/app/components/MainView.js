97880647068756ee7a1b16e56fe344e0ac2ba45c
Anastasia Mavridou <anastasia.mavridou@nasa.gov>
2020-06-01 19:00:38 -0700

    Added Delete Project functionality

--- fret-electron/app/components/MainView.js
+++ fret-electron/app/components/MainView.js
@@ -1,15 +1,15 @@
 // *****************************************************************************
 // Notices:
-// 
-// Copyright © 2019 United States Government as represented by the Administrator
+//
+// Copyright ï¿½ 2019 United States Government as represented by the Administrator
 // of the National Aeronautics and Space Administration.  All Rights Reserved.
-// 
+//
 // Disclaimers
-// 
+//
 // No Warranty: THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF
 // ANY KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED
-// TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS, 
-// ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, 
+// TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS,
+// ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
 // OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL BE
 // ERROR FREE, OR ANY WARRANTY THAT DOCUMENTATION, IF PROVIDED, WILL CONFORM TO
 // THE SUBJECT SOFTWARE. THIS AGREEMENT DOES NOT, IN ANY MANNER, CONSTITUTE AN
@@ -18,7 +18,7 @@
 // RESULTING FROM USE OF THE SUBJECT SOFTWARE.  FURTHER, GOVERNMENT AGENCY
 // DISCLAIMS ALL WARRANTIES AND LIABILITIES REGARDING THIRD-PARTY SOFTWARE, IF
 // PRESENT IN THE ORIGINAL SOFTWARE, AND DISTRIBUTES IT ''AS IS.''
-// 
+//
 // Waiver and Indemnity:  RECIPIENT AGREES TO WAIVE ANY AND ALL CLAIMS AGAINST
 // THE UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS
 // ANY PRIOR RECIPIENT.  IF RECIPIENT'S USE OF THE SUBJECT SOFTWARE RESULTS IN
@@ -67,10 +67,13 @@ import LanguageIcon from '@material-ui/icons/Language';
 import CloseIcon from '@material-ui/icons/Close';
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import NotesIcon from "@material-ui/icons/Notes";
+import DeleteIcon from '@material-ui/icons/Delete';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import css from './MainView.css';
 import CreateRequirementDialog from './CreateRequirementDialog';
 import CreateProjectDialog from './CreateProjectDialog';
+import DeleteProjectDialog from './DeleteProjectDialog';
 import AppMainContent from './AppMainContent';
 
 const app = require('electron').remote.app
@@ -170,7 +173,7 @@ const styles = theme => ({
 });
 
 
-// https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript/45035939 
+// https://stackoverflow.com/questions/19706046/how-to-read-an-external-local-json-file-in-javascript/45035939
 // Used in handleImport below
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
@@ -194,7 +197,9 @@ class MainView extends React.Component {
     mainContent: 'dashboard',
     selectedProject: 'All Projects',
     anchorEl: null,
-    listOfProjects: []
+    listOfProjects: [],
+    deleteProjectDialogOpen: false,
+    projectTobeDeleted: ''
   };
 
   handleImport = () => {
@@ -214,7 +219,7 @@ class MainView extends React.Component {
 	  // Version using "require" causes error: Cannot find module "."
 	  const filepathnoext = filepath.slice(0,-5); // The slice is to remove the .json suffix
 	  console.log('*** filepathnoext = ' + JSON.stringify(filepathnoext));
-	  data = require(filepathnoext); 
+	  data = require(filepathnoext);
 	  db.bulkDocs(data).catch((err) => {console.log(err);});
 	  */
 
@@ -379,6 +384,27 @@ class MainView extends React.Component {
     })
   }
 
+  handleDeleteProject = (name) => {
+    this.openDeleteProjectDialog(name)
+  }
+
+  openDeleteProjectDialog = (name) => {
+    this.setState({
+      deleteProjectDialogOpen: true,
+      projectTobeDeleted: name,
+      anchorEl: null
+    })
+
+  }
+
+  closeDeleteProjectDialog = () => {
+    this.setState({
+      deleteProjectDialogOpen: false,
+      projectTobeDeleted: '',
+      anchorEl: null
+    })
+  }
+
   render() {
     const { classes, theme } = this.props;
     const { anchorEl } = this.state;
@@ -428,10 +454,14 @@ class MainView extends React.Component {
                         this.state.listOfProjects.map(name => {
                           return <MenuItem
                                     key={name}
-                                    dense
-                                    onClick={() => this.handleSetProject(name)}>
+                                    dense>
                                     <ListItemIcon><NotesIcon color="secondary"/></ListItemIcon>
-                                    <ListItemText inset primary = {name} />
+                                    <ListItemText inset primary = {name} onClick={() => this.handleSetProject(name)}/>
+                                    <IconButton onClick={() => this.handleDeleteProject(name)} variant="flat" aria-label="delete" >
+                                      <Tooltip id="tooltip-icon-delete" title="Delete Project">
+                                      <DeleteIcon color='error'/>
+                                      </Tooltip>
+                                    </IconButton>
                                   </MenuItem>
                         })
                       }
@@ -522,7 +552,16 @@ class MainView extends React.Component {
             selectedProject={this.state.selectedProject}
             existingProjectNames={this.state.listOfProjects}
             />
-          <CreateProjectDialog open={this.state.createProjectDialogOpen} handleDialogClose={this.handleCreateProjectDialogClose} existingProjectNames={this.state.listOfProjects}/>
+          <CreateProjectDialog
+              open={this.state.createProjectDialogOpen}
+              handleDialogClose={this.handleCreateProjectDialogClose}
+              existingProjectNames={this.state.listOfProjects}
+          />
+          <DeleteProjectDialog
+            open={this.state.deleteProjectDialogOpen}
+            projectTobeDeleted={this.state.projectTobeDeleted}
+            handleDialogClose={this.closeDeleteProjectDialog}
+          />
         </div>
         <Snackbar
           anchorOrigin={{
