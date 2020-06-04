faf4e0be6fce6ce518e11d2891ff02173577ebb7
Anastasia Mavridou <anastasia.mavridou@nasa.gov>
2020-06-04 16:22:19 -0700

    Removed unnecessairy material ui components

--- fret-electron/app/components/LTLSimRequirementDetails.js
+++ fret-electron/app/components/LTLSimRequirementDetails.js
@@ -33,10 +33,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
-//import Card from '@material-ui/core/Card';
-//import CardActions from '@material-ui/core/CardActions';
-//import CardHeader from '@material-ui/core/CardHeader';
-//import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
