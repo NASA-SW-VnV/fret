eab4551b969ca5f21256cb966b402541a830fa35
Alain Leon <xerebz@gmail.com>
2021-04-30 10:45:07 -0700

    Update runfret.sh
    These lines blow away any FRET_HOME or NUSMV_HOME variables set in the environment making this file impossible to execute.

--- executables/runfret.sh
+++ executables/runfret.sh
@@ -1,11 +1,6 @@
 
 echo "running pre-compiled FRET"
 
-FRET_HOME=
-
-NUSMV_HOME=
-
-
 export PATH=$PATH:$NUSMV_HOME:$FRET_HOME:$FRET_HOME/resources/app/tools/LTLSIM/ltlsim-core/simulator/
 
 $FRET_HOME/FRET
