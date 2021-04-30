518b56342878d88a4f314a2d7cc1d3bfc917a610
Anastasia Mavridou <anmavrid@gmail.com>
2021-04-30 15:32:03 -0700

    Updated runfret and instructions for Linux executable

--- executables/runfret.sh
+++ executables/runfret.sh
@@ -1,9 +1,11 @@
 
 echo "running pre-compiled FRET"
 
-FRET_HOME=
+# Uncomment the line below and add the absolute path of the unpacked FRET-linux-64 directory. E.g., FRET_HOME=<path_to_FRET>
+# FRET_HOME=
 
-NUSMV_HOME=
+# Optionally, uncomment the line below and add the absolute path to the Linux NuSMV binaries directory. E.g., NuSMV_HOME=<path_to_NUSMV>
+# NUSMV_HOME=
 
 
 export PATH=$PATH:$NUSMV_HOME:$FRET_HOME:$FRET_HOME/resources/app/tools/LTLSIM/ltlsim-core/simulator/
