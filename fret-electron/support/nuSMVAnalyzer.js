89a8d38f4d2a6e4004f207d1067369fdc3afa5c0
Anastasia Mavridou <anmavrid@gmail.com>
2021-05-25 15:28:57 -0700

    FRET-234: Y turned to pre

--- fret-electron/support/nuSMVAnalyzer.js
+++ fret-electron/support/nuSMVAnalyzer.js
@@ -1,15 +1,15 @@
 // *****************************************************************************
 // Notices:
-// 
-// Copyright © 2019 United States Government as represented by the Administrator
+//
+// Copyright ï¿½ 2019 United States Government as represented by the Administrator
 // of the National Aeronautics and Space Administration.  All Rights Reserved.
-// 
+//
 // Disclaimers
-// 
+//
 // No Warranty: THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF
 // ANY KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED
-// TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS, 
-// ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, 
+// TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS,
+// ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
 // OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL BE
 // ERROR FREE, OR ANY WARRANTY THAT DOCUMENTATION, IF PROVIDED, WILL CONFORM TO
 // THE SUBJECT SOFTWARE. THIS AGREEMENT DOES NOT, IN ANY MANNER, CONSTITUTE AN
@@ -18,7 +18,7 @@
 // RESULTING FROM USE OF THE SUBJECT SOFTWARE.  FURTHER, GOVERNMENT AGENCY
 // DISCLAIMS ALL WARRANTIES AND LIABILITIES REGARDING THIRD-PARTY SOFTWARE, IF
 // PRESENT IN THE ORIGINAL SOFTWARE, AND DISTRIBUTES IT ''AS IS.''
-// 
+//
 // Waiver and Indemnity:  RECIPIENT AGREES TO WAIVE ANY AND ALL CLAIMS AGAINST
 // THE UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS
 // ANY PRIOR RECIPIENT.  IF RECIPIENT'S USE OF THE SUBJECT SOFTWARE RESULTS IN
@@ -87,7 +87,7 @@ nuSMVAnalyzer.prototype.visitProposition = function(ctx) {
 };
 
 nuSMVAnalyzer.prototype.visitSimpleExpr = function(ctx){
-  var result = '';
+  let result = '';
     this.visitChildren(ctx).forEach(child => {
       if (child != null && child != undefined)
         result += child;
@@ -97,7 +97,7 @@ nuSMVAnalyzer.prototype.visitSimpleExpr = function(ctx){
 
 // Visit a parse tree produced by NuSMVParser#simpleltl.
 nuSMVAnalyzer.prototype.visitSimpleltl = function(ctx) {
-  var result = '';
+  let result = '';
     this.visitChildren(ctx).forEach(child => {
       if (child != null && child != undefined)
         result += child;
@@ -107,7 +107,7 @@ nuSMVAnalyzer.prototype.visitSimpleltl = function(ctx) {
 
 // Visit a parse tree produced by NuSMVParser#unaryOp.
 nuSMVAnalyzer.prototype.visitUnaryPastOp = function(ctx) {
-  var operator = antlrUtilities.getText(ctx.pastUnaryOp(0));
+  let operator = antlrUtilities.getText(ctx.pastUnaryOp(0));
   if (operator === 'Y'){
     operator = 'pre ';
   }
@@ -121,16 +121,17 @@ nuSMVAnalyzer.prototype.visitUnaryPastOp = function(ctx) {
 
 // Visit a parse tree produced by NuSMVParser#timedUnarySaltPastOp.
 nuSMVAnalyzer.prototype.visitTimedUnarySaltPastOp = function(ctx) {
-  var result = antlrUtilities.getText(ctx.pastTimedUnaryOp(0));
-  var saltBound = antlrUtilities.getText(ctx.saltBound(0)).replace(/\s/g, '');
+  let result = antlrUtilities.getText(ctx.pastTimedUnaryOp(0));
+  let saltBound = antlrUtilities.getText(ctx.saltBound(0)).replace(/\s/g, '');
   saltBound = utilities.replaceSubstring(timingSubsts, saltBound);
-  result = result + 'T(' + saltBound + ','+antlrUtilities.getText(ctx.ltlExpr(0)) + ')';
-  return utilities.replaceSubstring(onceStrictSubs, result);
+  result = result + 'T(' + saltBound + ','+this.visit(ctx.ltlExpr(0)) + ')';
+  let finalresult = utilities.replaceSubstring(onceStrictSubs, result);
+  return finalresult;
 };
 
 // Visit a parse tree produced by NuSMVParser#binaryOp.
  nuSMVAnalyzer.prototype.visitBinaryPastOp = function(ctx) {
-   var result = antlrUtilities.getText(ctx.pastBinaryOp(0))+'( '+ this.visit(ctx.ltlExpr(1)) +', '+ this.visit(ctx.ltlExpr(0)) +' )';
+   let result = antlrUtilities.getText(ctx.pastBinaryOp(0))+'( '+ this.visit(ctx.ltlExpr(1)) +', '+ this.visit(ctx.ltlExpr(0)) +' )';
    return result;
 };
 
