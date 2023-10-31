c1a752b4be1121e76e2f7cf55de1a48973e5e336
Andreas Katis <andreas.katis@nasa.gov>
2023-10-31 15:14:36 -0400

    Added some troupe requirements for testing.

--- fret-electron/support/xform.js
+++ fret-electron/support/xform.js
@@ -859,11 +859,15 @@ function testObl(v,formula) {
 // testObl('a_0', formula)
 
 let formulas = {
+    'Default_State_Entry_Manual': '(H (((FSM_State_eq_Manual) & (Z (! (FSM_State_eq_Manual)))) -> (FSM_State_eq_Manual_RoverIdle_Idle)))',
+    'DZR_CharacterizeZone_Acquire_to_DZR_CharacterizeZone_Transmit': '(H ((Y (((FSM_State_eq_DZR_CharacterizeZone_Acquire) & ED_2) & (Z (! ((FSM_State_eq_DZR_CharacterizeZone_Acquire) & ED_2))))) -> ((FSM_State_eq_DZR_CharacterizeZone_Transmit) | (! (Y TRUE)))))',
+    'Event_Statusing_EA_2': '(H (EA_2 <-> ((H[0,3] (F_confirmationSentToNL | (F_confirmationReceivedFromNF & F_isLeader))) & (H[0,2] (Y TRUE)))))',
+    'Parameter_List_DnrSnrTransition_DZRCloseout_TransferData':'(H ((FSM_State_eq_DnrSnrTransition_DZRCloseout_TransferData) -> (((controllerType_eq_1) & (activity_eq_3)) & (velocity_eq_0))))'
     // 'FSM-001-fin' : '(LAST V ((((limits & (! standby)) & (! apfail)) & supported) -> pullup))',
     // 'FSM-001': '(G ((((limits & (! standby)) & (! apfail)) & supported) -> pullup))',
     // 'FSM-001-pltl': '(H ((((limits & (! standby)) & (! apfail)) & supported) -> pullup))',    
     // 'FSM-001-pltl-Y': '(Y ((((limits & (! standby)) & (! apfail)) & supported) -> pullup))',
-    'FSM-001v2-pltl': '(H (((autopilot & Y autopilot  & Y limits ) & (Y (! (autopilot & Y autopilot  & Y limits )) | FTP)) ->  (pullup)))',
+    //'FSM-001v2-pltl': '(H (((autopilot & Y autopilot  & Y limits ) & (Y (! (autopilot & Y autopilot  & Y limits )) | FTP)) ->  (pullup)))',
     // 'FSM-001v3-pltl': '(H ((((htlore3_autopilot & htlore3_notpreprelimits & Y limits ) & ((Y (! (htlore3_autopilot & htlore3_notpreprelimits & Y limits ))) | FTP)) ->  (pullup))))',
     // 'FSM-002': '(G ((standby & (state_eq_ap_transition_state)) -> (STATE_eq_ap_standby_state)))',
     // 'FSM-002-pltl': '(H ((standby & (state = ap_transition_state)) -> (STATE = ap_standby_state)))',
