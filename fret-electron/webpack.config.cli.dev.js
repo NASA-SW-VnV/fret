e4d9b76d0f41c5d0183ae857480567557e6200ea
Andreas Katis <andreas.katis@nasa.gov>
2024-02-29 14:19:52 -0500

    Added dev build scripts for CLI and adjusted start-cli-dev to run in development mode. Updated .gitignore to exclude dev build-related files.

---
+++ fret-electron/webpack.config.cli.dev.js
@@ -0,0 +1,56 @@
+/**
+ * Webpack config for production electron main process
+ */
+
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import baseConfig from './webpack.config.base';
+import CheckNodeEnv from './internals/scripts/CheckNodeEnv';
+
+CheckNodeEnv('development');
+
+export default merge.smart(baseConfig, {
+  devtool: 'eval-source-map',
+
+  target: 'node',
+
+  entry: './app/cli/fretCLI',
+
+  output: {
+    path: __dirname,
+    filename: './app/cli/fretCLI.main.dev.js'
+  },
+
+  plugins: [
+
+    new BundleAnalyzerPlugin({
+      analyzerMode: process.env.OPEN_ANALYZER === 'true' ? 'server' : 'disabled',
+      openAnalyzer: process.env.OPEN_ANALYZER === 'true'
+    }),
+
+    /**
+     * Create global constants which can be configured at compile time.
+     *
+     * Useful for allowing different behaviour between development builds and
+     * release builds
+     *
+     * NODE_ENV should be production so that modules do not perform certain
+     * development checks
+     */
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
+      DEBUG_PROD: 'false'
+    })
+  ],
+
+  /**
+   * Disables webpack processing of __dirname and __filename.
+   * If you run the bundle in node.js it falls back to these values of node.js.
+   * https://github.com/webpack/webpack/issues/2010
+   */
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+});
