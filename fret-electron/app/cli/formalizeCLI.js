ad6747f543e768b5934d45341901cf8f3e306875
Andreas Katis <andreas.katis@nasa.gov>
2024-02-29 19:31:17 -0500

    Improved error handling of parsing errors in formalize command.

--- fret-electron/app/cli/formalizeCLI.js
+++ fret-electron/app/cli/formalizeCLI.js
@@ -1,35 +1,39 @@
 import { compile } from '../parser/FretSemantics.js';
-import { printToConsole } from "./cliUtils";
+import { printProgramError, printToConsole } from "./cliUtils";
 export { formalizeCLI as formalizeCLI};
 
 
 function formalizeCLI(text, options) {
-    let formalizations = compile(text).collectedSemantics
-    let formalizationChoice;
-
-    switch (options.logic) {
-        case 'ft-inf':
-            formalizationChoice = 'ftInfAUExpanded';
-            break;
-        case 'ft-fin':
-            formalizationChoice = 'ftExpanded';
-            break;
-        case 'pt':
-            switch (languageChoice) {
-                case 'smv':
-                    formalizationChoice = 'ptExpanded';
-                    break;
-                case 'lustre':
-                    formalizationChoice = 'CoCoSpecCode';
-                    break;
-                default:
-                    formalizationChoice = 'ptExpanded';
-            }
-            break;
-        default:
-            formalizationChoice = 'ftInfAUExpanded';
-    }
+    let compiledText = compile(text)
+    if (compiledText.collectedSemantics) {
+        let formalizationChoice;
+        let formalizations = compiledText.collectedSemantics;
+        switch (options.logic) {
+            case 'ft-inf':
+                formalizationChoice = 'ftInfAUExpanded';
+                break;
+            case 'ft-fin':
+                formalizationChoice = 'ftExpanded';
+                break;
+            case 'pt':
+                switch (languageChoice) {
+                    case 'smv':
+                        formalizationChoice = 'ptExpanded';
+                        break;
+                    case 'lustre':
+                        formalizationChoice = 'CoCoSpecCode';
+                        break;
+                    default:
+                        formalizationChoice = 'ptExpanded';
+                }
+                break;
+            default:
+                formalizationChoice = 'ftInfAUExpanded';
+        }
     
-    let formalization = formalizations[formalizationChoice]        
-    printToConsole(null, formalization);
+        let formalization = formalizations[formalizationChoice]        
+        printToConsole(null, formalization);
+    } else {
+        printProgramError(new Error(compiledText.parseErrors));
+    }
 }
