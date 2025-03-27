a9eba0c14c3dee9841bbf4d8b0f469295338b54b
Anastasia Mavridou <anmavrid@gmail.com>
2025-03-27 14:12:27 -0700

    Commented out logging.

--- fret-electron/app/components/CreateProjectDialog.js
+++ fret-electron/app/components/CreateProjectDialog.js
@@ -1,7 +1,7 @@
 // Copyright © 2025, United States Government, as represented by the Administrator of the National Aeronautics and Space Administration. All rights reserved.
-// 
-// The “FRET : Formal Requirements Elicitation Tool - Version 3.0” software is licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. 
-// 
+//
+// The “FRET : Formal Requirements Elicitation Tool - Version 3.0” software is licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+//
 // Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -70,9 +70,9 @@ class CreateOrRenameProjectDialog extends React.Component {
     }
       // ipcRenderer call main with argLit and main returns result to update Redux store
     ipcRenderer.invoke(channel ,argList).then((result) => {
-      console.log('**CreateProjectDialog ipcRenderer channel result.listOfProjects: ', result.listOfProjects)
-      console.log('**CreateProjectDialog ipcRenderer channel: ', channel)
-      console.log('**CreateProjectDialog ipcRenderer channel result.requirements: ', result.requirements)
+      //console.log('**CreateProjectDialog ipcRenderer channel result.listOfProjects: ', result.listOfProjects)
+      //console.log('**CreateProjectDialog ipcRenderer channel: ', channel)
+      //console.log('**CreateProjectDialog ipcRenderer channel result.requirements: ', result.requirements)
 
       const defaultProject = result.listOfProjects.shift();
 
